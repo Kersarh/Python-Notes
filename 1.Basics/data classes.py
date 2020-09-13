4549d82dfe46749747403bf7453eac0ec54a0a39
Kersarh <kers3412.git@gmail.com>
2020-09-13 20:19:23 +0300

    dc

---
+++ 1.Basics/data classes.py
@@ -0,0 +1,32 @@
+from dataclasses import dataclass
+
+
+class name:
+    def __init__(self, name: str, age: int = 10):
+        self.name = name
+        self.age = age
+
+    def prnt_data(self) -> float:
+        print(self.name, self.age)
+
+
+name = name("user", 20)
+name.prnt_data()  # user 20
+print(name)  # <__main__.name object at 0x000001C87ED0A610>
+print("---------------------")
+
+
+#  с применением dataclass:
+@dataclass
+class name2:
+    name: str
+    age: int = 10
+
+    def prnt_data(self) -> float:
+        print(self.name, self.age)
+
+
+name = name2("user", 20)  # user 20
+name.prnt_data()  # name2(name='user', age=20)
+print(name)
+print("---------------------")
