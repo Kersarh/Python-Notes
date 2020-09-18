b46ce61149150268954cbd9abfa00b88139e8cbe
Kersarh <kers3412.git@gmail.com>
2020-09-18 10:21:04 +0300

    enum

---
+++ 1.Basics/14.enum_range.py
@@ -0,0 +1,35 @@
+'''
+Функция enumerate(<Объект>) на каждой итерации цикла for
+возвращает кор-теж из индекса и значения текущего элемента
+'''
+arr2 = [1, 2, 3, 4, 5, 6]
+for i, elem in enumerate(arr2):
+    print("i=", i, "elem=", elem)
+
+print(enumerate(arr2))  # <enumerate object at 0x000001D9B298C600>
+
+# range
+'''
+range(arg) может быть очень полезной, если вам нужно выполнить действие определенное количество раз.
+'''
+arr = [1, 2, 3]
+
+# по длинне arr
+for i in range(len(arr)):
+    print(i)
+print("---------------")
+
+# range от 1 до 5 выведет 1-4
+for i in range(1, 5):
+    print(i)
+print("---------------")
+
+# range от 10 до 0 с шагом -1 выведет 5-1
+for i in range(5, 0, -1):
+    print(i)
+print("---------------")
+
+# range от 2 до 15 с шагом 3 выведет 2 5 8 11 14
+for i in range(2, 15, 3):
+    print(i)
+print("---------------")
