3b47980d43610cad6ba68e357db56f6c03a5940b
Kersarh <kers3412.git@gmail.com>
2020-10-01 11:19:27 +0300

    whl

--- 2.Advansed/Create_Package/project/setup.py
+++ 2.Advansed/Create_Package/project/setup.py
@@ -1,19 +1,25 @@
-from setuptools import setup, find_packages
+'''
+setup.py - параметры для сборки пакетов
+'''
+
+#from setuptools import setup
+from distutils.core import setup  # для whl
+from setuptools import find_packages
 from os.path import join, dirname
 
 setup(
     name="MyPackage",
-    version="1.0", # Указать в файле __init__.py удалить здесь
+    version="1.0",  # Указать в файле __init__.py удалить здесь
     description="Package Description",
     long_description=open(join(dirname(__file__), "README.md")).read(),
     url="http://example.com",
     packages=find_packages(),
-    include_package_data=True, # Подключить файлы из MANIFEST.in
+    include_package_data=True,  # Подключить файлы из MANIFEST.in
     install_requires=[],  # Зависимости
-    entry_points={  
+    entry_points={
         "console_scripts": ["mypckg = module1.main:print_message"]
         # Теперь можно вызвать mypckg в консоли.
         # Будет выволнен module1.main:print_message
     },
-    test_suite='tests', # Подключаем тесты 'python setup.py test'
+    test_suite='tests',  # Подключаем тесты 'python setup.py test'
 )
