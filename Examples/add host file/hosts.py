36db23a4e66ae77dbc4e9799a739405ab131fb27
Kersarh <kers3412.git@gmail.com>
2020-12-13 23:44:40 +0300

    file

--- Examples/add host file/hosts.py
+++ Examples/add host file/hosts.py
@@ -1,47 +1,48 @@
-import os
+"""
+Скрипт записывает уникальные строки из new_data_file в test.txt.
+Запись данных происходит в начало файла
+"""
+# Файлы
+new_data_file = "base.txt"  # Откуда берутся данные
+work_file = "test.txt"  # Куда добавляются
 
-# Пути к файлам
-# windir = os.getenv('WINDIR')
-# raw_file = windir + "\\System32\\drivers\\etc\\hosts"
-base_file = "base.txt"
-raw_file = "test.txt"
+# Читаем new_data_file в список
+# .splitlines() разбивает на строки без символа \n
+new_data_list = []
+with open(new_data_file, "r") as nd:
+    new_data_list = nd.read().splitlines()
 
-# Читаем файлы в списки
-b_ls = []
-with open(base_file, "r") as bf:
-    b_ls = bf.read().splitlines()
-    # .splitlines() разбивает на строки без символа \n
-
-r_ls = []
-with open(raw_file, "r+") as rf:
-    r_ls = rf.read().splitlines()
-rf.close()
+work_list = []
+with open(work_file, "r") as wl:
+    work_list = wl.read().splitlines()
 
 # Сортируем списки выбираем недостающие записи
-ss_ls = []
-for i in b_ls:
-    if not i in r_ls:
+new_list = []
+for i in new_data_list:
+    if not i in work_list:
         if len(i) == 0:
             continue
-        ss_ls.append(i)
+        new_list.append(i)
 
 # Отобразить что добавляем
-for i in ss_ls:
-    print("отсутствует ", i)
+for i in new_list:
+    print("Отсутствует ", i)
 
 # Записываем в файл
-if ss_ls:
-    print("Add data to file")
-    of = open(raw_file, "a+")
-    of.write("\n--- open new block ---\n")
-    for i in ss_ls:
-        of.write(i + "\n")
-    of.write("\n--- close new block ---\n")
-    of.close()
-elif not ss_ls:
-    print("Not new data")
+if new_list:
+    print("Добавляем данные в файл")
+    # Двойное открытие файла чтобы писать в начало
+    # Если нужно писать в конец оставить только второе открытие файла
+    # И прописать модификатор доступа a+
+    with open(work_file, "r") as wl:
+        file = wl.read()
+    with open(work_file, "w") as wl:
+        wl.write("# --- Начало блока ---\n")
+        for i in new_list:
+            wl.write(i + "\n")
+        wl.write("# --- Конец блока ---\n")
+        wl.write(file)
+elif not new_list:
+    print("Нет новых данных")
 else:
-    print("error")
-
-os.system("pause" if os.name == "nt" else
-          """bash -c 'read -s -n 1 -p "Press any key to continue...\n"'""")
+    print("Ошибка")
