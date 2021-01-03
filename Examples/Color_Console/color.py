d2ae9974506c73c07cdf9c9a926511df3ffa7d94
Kersarh <kers3412.git@gmail.com>
2021-01-03 17:55:10 +0300

    color

---
+++ Examples/Color_Console/color.py
@@ -0,0 +1,12 @@
+import os
+import colorama
+
+colorama.init()
+
+print(colorama.Fore.RED + 'some red text')
+print(colorama.Fore.GREEN + 'and with a green background')
+print(colorama.Style.DIM + 'and in dim text')
+print(colorama.Style.RESET_ALL)
+print('back to normal now')
+
+os.system("pause")
