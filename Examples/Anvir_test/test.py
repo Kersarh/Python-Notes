1c985ea8afe4cd0d0aec1c97124f1c0911a813c1
Kersarh <kers3412.git@gmail.com>
2021-01-03 17:57:23 +0300

    vir

---
+++ Examples/Anvir_test/test.py
@@ -0,0 +1,9 @@
+"""
+Создает тестовый текстовый файл опредяляемый антивирусами как вирус!
+"""
+str1 = """X5O!P%@AP[4\PZX54(P^)7CC)7}$EICAR-"""
+str2 = "STANDARD-ANTIVIRUS-TEST-FILE!"
+str3 = "$H+H*"
+
+with open("test.txt", "w") as f:
+	f.write(str1+str2+str3)
