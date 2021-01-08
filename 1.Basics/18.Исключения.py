092d450fb4f3f2681d3cf8bd5a04c272b6520ad6
Kersarh <kers3412.git@gmail.com>
2021-01-09 00:27:19 +0300

    except

---
+++ 1.Basics/18.Исключения.py
@@ -0,0 +1,92 @@
+'''
+Использование исключений.
+'''
+import sys
+try:
+	#Код программы
+	# raise ExceptionType - принудительный вызов исключений
+	# raise Exception("Исключение через raise")
+	i = 2/0 # Делить на 0 нельзя
+	print(i)
+except ZeroDivisionError as e:
+	# Ловим исключение деления на ноль
+	print("Нельзя делить на ноль!", e)
+	sys.exit() # Принудительный выход из программы
+except Exception as e:
+	# Ловим любое исключение
+	print("Другое исключение! ", e)
+else:
+	# Сработает если исключения небыло
+	print("OK!")
+finally:
+	# Код который сработает всегда
+	# независимо было исключение или нет
+	print("Этот код все равно сработает!")
+
+print("Продолжение программы")
+
+# Иерархия исключений  https://docs.python.org/3/library/exceptions.html
+# BaseException
+#  +-- SystemExit
+#  +-- KeyboardInterrupt
+#  +-- GeneratorExit
+#  +-- Exception
+#       +-- StopIteration
+#       +-- StopAsyncIteration
+#       +-- ArithmeticError
+#       |    +-- FloatingPointError
+#       |    +-- OverflowError
+#       |    +-- ZeroDivisionError
+#       +-- AssertionError
+#       +-- AttributeError
+#       +-- BufferError
+#       +-- EOFError
+#       +-- ImportError
+#       |    +-- ModuleNotFoundError
+#       +-- LookupError
+#       |    +-- IndexError
+#       |    +-- KeyError
+#       +-- MemoryError
+#       +-- NameError
+#       |    +-- UnboundLocalError
+#       +-- OSError
+#       |    +-- BlockingIOError
+#       |    +-- ChildProcessError
+#       |    +-- ConnectionError
+#       |    |    +-- BrokenPipeError
+#       |    |    +-- ConnectionAbortedError
+#       |    |    +-- ConnectionRefusedError
+#       |    |    +-- ConnectionResetError
+#       |    +-- FileExistsError
+#       |    +-- FileNotFoundError
+#       |    +-- InterruptedError
+#       |    +-- IsADirectoryError
+#       |    +-- NotADirectoryError
+#       |    +-- PermissionError
+#       |    +-- ProcessLookupError
+#       |    +-- TimeoutError
+#       +-- ReferenceError
+#       +-- RuntimeError
+#       |    +-- NotImplementedError
+#       |    +-- RecursionError
+#       +-- SyntaxError
+#       |    +-- IndentationError
+#       |         +-- TabError
+#       +-- SystemError
+#       +-- TypeError
+#       +-- ValueError
+#       |    +-- UnicodeError
+#       |         +-- UnicodeDecodeError
+#       |         +-- UnicodeEncodeError
+#       |         +-- UnicodeTranslateError
+#       +-- Warning
+#            +-- DeprecationWarning
+#            +-- PendingDeprecationWarning
+#            +-- RuntimeWarning
+#            +-- SyntaxWarning
+#            +-- UserWarning
+#            +-- FutureWarning
+#            +-- ImportWarning
+#            +-- UnicodeWarning
+#            +-- BytesWarning
+#            +-- ResourceWarning
