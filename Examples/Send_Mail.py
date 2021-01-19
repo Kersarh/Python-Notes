75e44a6d8d5b672d40952fd170797ddc18a5f8d9
Kersarh <kers3412.git@gmail.com>
2021-01-19 23:03:58 +0300

    mail

--- Examples/Send_Mail.py
+++ Examples/Send_Mail.py
@@ -1,19 +1,14 @@
 # -*- coding: utf-8 -*-
 
 import smtplib
-import email
-import mimetypes
 import os
 
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEBase
 from email.mime.multipart import MIMEMultipart
-from email.mime.image import MIMEImage
-from email.mime.audio import MIMEAudio
 from email.header import Header
-from base64 import encodebytes
 
-# Параметры SMTP-сервера
+# Данные письма
 mail_from = "from@gmail.com"  # отправитель
 mail_to = "to@gmail.com"  # получатель
 mail_text = "Тестовое письмо!nПослано из python"  # текст письма
@@ -21,12 +16,13 @@ mail_subj = "Тестовое письмо"  # заголовок письма
 mail_coding = "windows-1251"
 attach_file = ""  # ["D:\\test.txt", "D:\\test2.txt"] # прикрепляемый файл
 
+# Параметры SMTP-сервера
 smtp_server = "smtp.gmail.com"
 smtp_port = 587
 smtp_user = "from@gmail.com"  # пользователь smtp
 smtp_pwd = "pass"  # пароль smtp
 
-# формирование сообщения
+# Формирование сообщения
 multi_msg = MIMEMultipart()
 multi_msg["From"] = Header(mail_from, mail_coding)
 multi_msg["To"] = Header(mail_to, mail_coding)
@@ -61,5 +57,11 @@ smtp.login(smtp_user, smtp_pwd)
 smtp.sendmail(mail_from, mail_to, multi_msg.as_string())
 smtp.quit()
 
-os.system("pause" if os.name == "nt" else
-          """bash -c 'read -s -n 1 -p "Press any key to continue...\n"'""")
+
+'''
+Разрешить авторизацию
+Google 
+https://www.google.com/settings/security/lesssecureapps
+Яндекс
+https://mail.yandex.ru/?dpda=yes#setup/client
+'''
