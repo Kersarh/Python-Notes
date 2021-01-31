144a557104a6e48a1d166d6acc0560bcfc88fd06
Kersarh <kers3412.git@gmail.com>
2021-02-01 00:33:08 +0300

    keyb_for_win

--- Examples/Клавиатура/Нажатие клавиши.py
+++ Examples/Клавиатура/Нажатие клавиши.py
@@ -5,3 +5,9 @@ for i in range(5):
     keyboard.press('a') # Напечатает a
     time.sleep(0.1)
 keyboard.release('a')
+
+# Вариант только для Windows
+# import msvcrt
+# key = ord(msvcrt.getch()) 
+# if key == 27: 
+#     print("ESC")
