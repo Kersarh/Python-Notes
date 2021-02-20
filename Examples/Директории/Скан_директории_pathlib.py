6e9ab70c8928e02efa0667306bc51427de24093c
Kersarh <kers3412.git@gmail.com>
2021-02-20 13:27:35 +0300

    scan_dir

---
+++ Examples/Директории/Скан_директории_pathlib.py
@@ -0,0 +1,10 @@
+"""
+Сканирует директории и субдиректории и выводит фсе файлы и их размер в кб
+"""
+from pathlib import Path
+
+root_path = Path('D:')
+
+for path in Path.rglob(root_path, '*'):
+    if path.is_file():
+        print(f"{path} {path.stat().st_size / 1024:.3f} Кб")
