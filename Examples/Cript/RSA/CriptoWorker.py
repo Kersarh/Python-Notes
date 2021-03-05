64f71cf268187b0a4ee98d2794bd557eec18fc95
Kersarh <kers3412.git@gmail.com>
2021-03-05 23:04:55 +0300

    CriptoWorker

--- Examples/Cript/RSA/CriptoWorker.py
+++ Examples/Cript/RSA/CriptoWorker.py
@@ -1,5 +1,5 @@
-import pickle
 import os
+import pickle
 
 from Crypto import Random
 from Crypto.Cipher import AES, PKCS1_OAEP
@@ -12,22 +12,19 @@ from Crypto.Signature import PKCS1_v1_5
 # pycryptodome==3.10.1
 
 
-class CriptoWorker():
+class CriptoWorker:
+    """
+    Класс шифрования RSA. Надстройка над pycryptodome
+    """
 
-    a = ""
-    b = ""
     def __init__(self) -> None:
         pass
 
-    def createKey(self,
-                  savefolder: str = "keys",
-                  privateKeyPwd: bool = False,
-                  secret: str = "secret") -> dict:
+    def createKey(self, savefolder: str = "keys", secret: str = None) -> dict:
         """Генерация пары RSA ключей
 
         Args:
             savefolder (str, optional): [Директория сохранения ключей]. Defaults to "keys".
-            privateKeyPwd (bool, optional): [Использование пароля на приватном ключе]. Defaults to False.
             secret (str, optional): [Пароль приватного ключа]. Defaults to "secret".
 
         Returns:
@@ -42,31 +39,36 @@ class CriptoWorker():
         key = RSA.generate(2048)
 
         # Приватный ключ
-        if privateKeyPwd == True:  # с паролем
-            private_key = key.export_key(passphrase=secret,
-                                         pkcs=8,
-                                         protection="scryptAndAES128-CBC")
-        elif privateKeyPwd == False:  # без пароля
-            private_key = key.export_key('PEM')
+        if secret:  # с паролем
+            private_key = key.export_key(
+                passphrase=secret, pkcs=8, protection="scryptAndAES128-CBC"
+            )
+        else:  # без пароля
+            private_key = key.export_key("PEM")
 
         # Сохраняем приватный ключ
-        private_file = f'{savefolder}\\private.pem'
-        with open(private_file, 'wb') as f:
+        private_file = f"{savefolder}\\private.pem"
+        with open(private_file, "wb") as f:
             f.write(private_key)
 
         # Публичный ключ
         # Генерируем
-        public_key = key.publickey().export_key('PEM')
+        public_key = key.publickey().export_key("PEM")
         # записываем в файл
-        public_file = f'{savefolder}\\public.pem'
-        with open(public_file, 'wb') as f:
+        public_file = f"{savefolder}\\public.pem"
+        with open(public_file, "wb") as f:
             f.write(public_key)
         # Возвращаем список ключей
         result = {"private": private_key, "public": public_key}
         return result
 
-    def createSignature(self, raw_message: bytes, aliseprivate: bytes,
-                        bobpublic: bytes) -> bytes:
+    def createSignature(
+        self,
+        raw_message: bytes,
+        aliseprivate: bytes,
+        bobpublic: bytes,
+        privatePwd: str = None,
+    ) -> bytes:
         """Создает сигнатуру сообщения.
 
         Алиса подписывает сообщение своей цифровой подписью и шифрует ее открытым ключом Боба (асимметричным алгоритмом RSA).
@@ -75,13 +77,14 @@ class CriptoWorker():
             raw_message (bytes): [Шифруемое сообщение]
             aliseprivate (bytes): [Приватный ключ отправителя]
             bobpublic (bytes): [Публичный ключ адресата]
+            privatePwd (str, optional): [Пароль приватного ключа]. Defaults to None.
 
         Returns:
-            bytes: [Сигнатура сообщения]
+            bytes: [description]
         """
         plaintext = raw_message
         # Подписываем цифровой подписью Алисы
-        privatekey = RSA.importKey(aliseprivate)
+        privatekey = RSA.importKey(aliseprivate, passphrase=privatePwd)
         myhash = SHA.new(plaintext)
         signature = PKCS1_v1_5.new(privatekey)
         signature = signature.sign(myhash)
@@ -92,8 +95,14 @@ class CriptoWorker():
         sig = sig + cipherrsa.encrypt(signature[128:])
         return bytes(sig)
 
-    def checkSignature(self, message: dict, decript_message: bytes,
-                       bobprivate: bytes, alisepublic: bytes) -> bool:
+    def checkSignature(
+        self,
+        message: dict,
+        decript_message: bytes,
+        bobprivate: bytes,
+        alisepublic: bytes,
+        privatePwd=None,
+    ) -> bool:
         """Проверка сигнатуры.
 
         Боб расшифровывает и проверяет подпись Алисы.
@@ -103,6 +112,7 @@ class CriptoWorker():
             decript_message (bytes): [Расшифрованное сообщение]
             bobprivate (bytes): [Приватный ключ получателя]
             alisepublic (bytes): [Публичный ключ отправителя]
+            privatePwd (str, optional): [Пароль приватного ключа]. Defaults to None.
 
         Returns:
             bool: [True или False]
@@ -110,7 +120,7 @@ class CriptoWorker():
 
         # Расшифровываем сигнатуру
         signature = message["Signature"]
-        privatekey = RSA.importKey(bobprivate)
+        privatekey = RSA.importKey(bobprivate, passphrase=privatePwd)
         cipherrsa = PKCS1_OAEP.new(privatekey)
         sig = cipherrsa.decrypt(signature[:256])
         sig = sig + cipherrsa.decrypt(signature[256:])
@@ -123,8 +133,9 @@ class CriptoWorker():
         test = signature.verify(myhash, sig)
         return test
 
-    def createMsg(self, raw_message: bytes, aliseprivate: bytes,
-                  bobpublic: bytes) -> dict:
+    def createMsg(
+        self, raw_message: bytes, aliseprivate: bytes, bobpublic: bytes, privatePwd=None
+    ) -> dict:
         """Создание зашифрованного сообщения.
 
         Алиса генерирует случайный сеансовый ключ и шифрует этим ключом сообщение (с помощью симметричного алгоритма AES).
@@ -134,6 +145,7 @@ class CriptoWorker():
             raw_message (bytes): [Сообщение для шифровки]
             aliseprivate (bytes): [Приватный ключ отправителя]
             bobpublic (bytes): [Публичный ключ получателя]
+            privatePwd (str, optional): [Пароль приватного ключа]. Defaults to None.
 
         Returns:
             dict: [Словарь из "Data" - Сообщение, "Signature" - Сигнатуры, "Sessionkey" - Сессионного ключа]
@@ -159,7 +171,10 @@ class CriptoWorker():
         sessionkey = bytes(sessionkey)  # Сессионный ключ
 
         # Создаем сигнатуру
-        signature = self.createSignature(raw_message, aliseprivate, bobpublic)
+        aliseprivate_key = RSA.importKey(aliseprivate, passphrase=privatePwd)
+        signature = self.createSignature(
+            raw_message, aliseprivate, bobpublic, privatePwd=privatePwd
+        )
 
         result = {
             "Data": cript_message,
@@ -168,25 +183,27 @@ class CriptoWorker():
         }
         return result
 
-    def decryptData(self, message: dict, bobprivate: bytes,
-                    alisepublic: bytes) -> list:
+    def decryptData(
+        self, message: dict, bobprivate: bytes, alisepublic: bytes, privatePwd=None
+    ) -> list:
         """Дешифрование данных.
 
         Боб расшифровывает сеансовый ключ своим приватным ключом.
         При помощи полученного, таким образом, сеансового ключа
-        Боб расшифровывает зашифрованное сообщение Алисы 
+        Боб расшифровывает зашифрованное сообщение Алисы
         и проверяет сигнатуру
 
         Args:
             message (dict): [Зашифрованный файл]
             bobprivate (bytes): [Приватный ключ получателя]
             alisepublic (bytes): [Публичный ключ отправителя]
+            privatePwd (str, optional): [Пароль приватного ключа]. Defaults to None.
 
         Returns:
             list: [Список из расшифрованного сообщения и отметки верификации]
         """
 
-        privatekey = RSA.importKey(bobprivate)
+        privatekey = RSA.importKey(bobprivate, passphrase=privatePwd)
         cipherrsa = PKCS1_OAEP.new(privatekey)
         # Расшифровка сеансового ключа
         sessionkey = message["Sessionkey"]
@@ -200,28 +217,30 @@ class CriptoWorker():
         plaintext = obj.decrypt(ciphertext)
         plaintext = plaintext[16:]
         # Провека сигнатуры
-        verification = self.checkSignature(message, plaintext, bobprivate,
-                                           alisepublic)
+        verification = self.checkSignature(message, plaintext, bobprivate, alisepublic, privatePwd=privatePwd)
         return [plaintext, verification]
 
     def saveMsgToFile(self, message: dict, file: str) -> None:
-        with open(file, 'wb') as f:
+        with open(file, "wb") as f:
             pickle.dump(message, f)
 
     def readFileToMsg(self, file: str) -> None:
-        with open(file, 'rb') as f:
+        with open(file, "rb") as f:
             data_new = pickle.load(f)
         return data_new
 
 
-message = "Тесовое сообщение".encode()
-cript = CriptoWorker()
-alise = cript.createKey("alise")
-bob = cript.createKey("bob")
-cripto_mes = cript.createMsg(message, alise["private"], bob["public"])
+if __name__ == "__main__":
+    message = "Тесовое сообщение".encode()
+    cript = CriptoWorker()
+    alise = cript.createKey("alise", secret="my_password")
+    bob = cript.createKey("bob", secret="my_password")
+    cripto_mes = cript.createMsg(
+        message, alise["private"], bob["public"], privatePwd="my_password"
+    )
 
-cript.saveMsgToFile(cripto_mes, "file.txt")
-a = cript.readFileToMsg("file.txt")
-print(cripto_mes == a)
-d = cript.decryptData(a, bob["private"], alise["public"])
-print(f"Соотбщение: {d[0].decode()} \nВерификация: {d[1]}")
+    cript.saveMsgToFile(cripto_mes, "file.txt")
+    a = cript.readFileToMsg("file.txt")
+    print(cripto_mes == a)
+    d = cript.decryptData(a, bob["private"], alise["public"], privatePwd="my_password")
+    print(f"Сообщение: {d[0].decode()} \nВерификация: {d[1]}")
