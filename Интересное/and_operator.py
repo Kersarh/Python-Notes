67b4b326d9c2773920eacf0c8e2cb5e90ea1dd6f
Kersarh <kers3412.git@gmail.com>
2021-09-27 11:49:44 +0300

    and

---
+++ Интересное/and_operator.py
@@ -0,0 +1,15 @@
+
+print(True and True) # True
+print(False and False) #False
+print(True and False) # False
+print(False and True) # False
+
+
+print(1 and 5) # 5
+print(5 and 1) # 1
+
+print("hello" and 3) # 3
+print(3 and "hello") # hello
+ 
+print(5 or 2 and 2 > 1) # 5
+print((5 or 2) and 2 > 1) # True
